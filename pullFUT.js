2fdefe9f1cf000d38a04b2f154011b2925826650
dcowen91 <dcowen91@email.arizona.edu>
2015-07-30 00:12:07 -0700

    init

---
+++ pullFUT.js
@@ -0,0 +1,26 @@
+
+var http = require('http');
+var bl = require('bl')
+var baseUrl =  'http://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:{0}%7D';
+
+// console.log(baseUrl.replace("{0}", 1));
+var result = '';
+
+http.get(baseUrl, function(response)
+{
+	response.setEncoding("utf8");
+	response.on('data', concatData);
+	response.on('error', console.error);
+	response.on('end', handleData)
+})
+
+function concatData(data)
+{
+	result += data.toString();
+}
+
+function handleData()
+{
+	var jsonBlob = JSON.parse(result)
+	console.log(jsonBlob.totalPages);
+}
