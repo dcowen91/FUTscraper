3850ec5273ba02926bb37065ecb559ac4e81dda1
dcowen91 <dcowen91@email.arizona.edu>
2015-07-30 20:43:19 -0700

    package, more requests

--- pullFUT.js
+++ pullFUT.js
@@ -1,9 +1,9 @@
 
 var http = require('http');
-var bl = require('bl')
+var bl = require('bl');
 
 
-var baseUrl =  'http://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:{0}%7D';
+var baseUrl =  'http://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=';
 var totalPages = 0;
 var result = '';
 
@@ -12,16 +12,43 @@ http.get(baseUrl, function(response)
 	response.setEncoding("utf8");
 	response.on('data', concatData);
 	response.on('error', console.error);
-	response.on('end', handleData)
-})
+	response.on('end', handlePageCount);
+});
 
 function concatData(data)
 {
 	result += data.toString();
 }
 
-function handleData()
+function handlePageCount()
 {
-	var jsonBlob = JSON.parse(result)
-	console.log(totalPages = jsonBlob.totalPages);
+	var jsonBlob = JSON.parse(result);
+	result = '';
+	totalPages = jsonBlob.totalPages;
+	console.log(totalPages + "pages");
+	getAllPages();
+}
+
+function getAllPages()
+{
+	//add promise so this is isnt broken
+	for (var i = 1; i <= 1; i++)
+	{
+		var pages = {page : i};
+		var curUrl = baseUrl + JSON.stringify(pages);
+		http.get(curUrl, function(response)
+		{
+			response.setEncoding("utf8");
+			response.on('data', concatData);
+			response.on('error', console.error);
+			response.on('end', concatJson);
+		});
+	}
+}
+
+function concatJson()
+{
+	var jsonblob = JSON.parse(result);
+	result = '';
+	console.log(jsonblob.items[0].rating + " rated " + jsonblob.items[0].name);
 }
