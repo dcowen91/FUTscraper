7fecd64ca876b6ce4b5111ff99d62a876ae7924b
dcowen91 <dcowen@email.arizona.edu>
2016-08-01 22:05:05 -0700

    clean up code

--- pullFUT.js
+++ pullFUT.js
@@ -7,32 +7,34 @@ var baseUrl =  'http://www.easports.com/fifa/ultimate-team/api/fut/item?jsonPara
 var totalPages = 0;
 var arr = [];
 
-http.get(baseUrl, function(response)
+http.get(baseUrl + JSON.stringify({ ovr: "74:99"}), function(response)
 {
 	response.setEncoding("utf8");
-	response.pipe(bl(getPageCount))
+	response.pipe(bl(getPageCountAndFetchData))
 });
 
-function getPageCount(err, data)
+function getPageCountAndFetchData(err, data)
 {
 	totalPages = JSON.parse(data.toString()).totalPages;
-	console.log(totalPages);
-	//716
-	getPage(1);
+	if (totalPages && totalPages > 0)
+	{
+		console.log(totalPages);
+		fetchPagesAndSerialize(1);
+	}
 }
 
-function getPage(index)
+function fetchPagesAndSerialize(index)
 {
 	console.log(index);
-	var pages = {page : index};
-	var curUrl = baseUrl + JSON.stringify(pages);
-	http.get(curUrl, function(response)
+	var params = {page : index, ovr: "74:99"};
+	var currrentUrl = baseUrl + JSON.stringify(params);
+	http.get(currrentUrl, function(response)
 	{
 		response.setEncoding("utf8");
 		response.pipe(bl(function(err, data)
 		{
-			var current =JSON.parse(data.toString());
-			arr.push(current.items);
+			var currentData =JSON.parse(data.toString());
+			arr.push(currentData.items);
 
 			if (arr.length == totalPages - 1)
 			{
@@ -40,7 +42,7 @@ function getPage(index)
 			}
 			else 
 			{
-				getPage(index +1);
+				fetchPagesAndSerialize(index +1);
 			}
 		}))
 	});
